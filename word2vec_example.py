a9d33a41381dc4c7d698cfb1d0ed019380a14889
dk77.yun <dk77.yun@samsung.com>
2019-01-17 07:37:04 +0900

    update word2vec example
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

--- word2vec_example.py
+++ word2vec_example.py
@@ -1,29 +1,105 @@
 
-
+# https://www.youtube.com/watch?v=sY4YyacSsLc
+# https://github.com/minsuk-heo/python_tutorial/blob/master/data_science/nlp/word2vec_tensorflow.ipynb
 # from https://machinelearningmastery.com/develop-word-embeddings-python-gensim/
 
+# https://radimrehurek.com/gensim/models/word2vec.html
+# For reproducibility between interpreter launches
+#  - word2vec with seed value & workers = 1
+#  - In Python 3, requires use of the PYTHONHASHSEED environment 
+#    - ex: export PYTHONHASHSEED=777
+
+import sys
 from gensim.models import Word2Vec
-# define training data
-sentences = [['this', 'is', 'the', 'first', 'sentence', 'for', 'word2vec'],
-			['this', 'is', 'the', 'second', 'sentence'],
-			['yet', 'another', 'sentence'],
-			['one', 'more', 'sentence'],
-			['and', 'the', 'final', 'sentence']]
+
+corpus = ['king is a strong man',
+          'queen is a wise woman',
+          'boy is a young man',
+          'girl is a young woman',
+          'prince is a young king',
+          'princess is a young queen',
+          'man is strong',
+          'woman is pretty',
+          'prince is a boy will be king',
+          'princess is a girl will be queen']
+
+def remove_stop_words(corpus):
+    stop_words = ['is', 'a', 'will', 'be']
+    results = []
+    for text in corpus:
+        tmp = text.split(' ')
+        for stop_word in stop_words:
+            if stop_word in tmp:
+                tmp.remove(stop_word)
+        results.append(" ".join(tmp))
+    
+    return results
+
+tmp = remove_stop_words(corpus)
+print('-'*20)
+print(tmp)
+
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.corpus import stopwords
+
+import nltk
+nltk.download('stopwords')
+
+stopWords = set(stopwords.words('english'))
+def remove_stop_words_nltk(corpus):
+    results = []
+    for text in corpus:
+        tmp = text.split(' ')
+        for stop_word in stopWords:
+            if stop_word in tmp:
+                tmp.remove(stop_word)
+        results.append(" ".join(tmp))
+    
+    return results
+
+tmp = remove_stop_words_nltk(corpus)
+print('-'*20)
+print(tmp)
+
+corpus = tmp
+
+sentences = []
+for sentence in corpus:
+    sentences.append(sentence.split())
+print(sentences)
+
+'''
+sentences = [["king","strong","man"],
+            ["queen","wise","woman"],
+            ["boy","young","man"],
+            ["girl","young","woman"],
+            ["prince","young","king"],
+            ["princess","young","queen"],
+            ["man","strong"],
+            ["woman","pretty"],
+            ["prince","boy","king"],
+            ["princess","girl","queen"]]
+'''
+
 # train model
 model = Word2Vec(sentences
+    , size = 200
     , min_count=1
     , window = 1
-    , sg=1)
+    , sg=1
+    , seed = 777
+    , workers = 1)
 # summarize the loaded model
 print(model)
 # summarize vocabulary
 words = list(model.wv.vocab)
 print(words)
 # access vector for one word
-print(model['sentence'])
+print(model['king'])
 
-#result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
-result = model.most_similar("the")
+result = model.most_similar(positive=['woman', 'king'], negative=['man'], topn=1)
+print(result)
+result = model.most_similar("king")
 print(result)
 
 # save model
